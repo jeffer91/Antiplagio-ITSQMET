4d648d73879d208f7b9f6399ecde4ccaa77b546e
jeffer91 <jeffersonvillarreal91@gmail.com>
2026-08-14 11:42:45 -0500

    feat: expose report export bridge

--- electron/preload.ts
+++ electron/preload.ts
@@ -1,6 +1,8 @@
-import { contextBridge } from 'electron';
+import { contextBridge, ipcRenderer } from 'electron';
 
 contextBridge.exposeInMainWorld('siaiDesktop', {
   platform: process.platform,
   electronVersion: process.versions.electron,
+  savePdf: (html: string, defaultFileName: string) => ipcRenderer.invoke('siai:save-pdf', { html, defaultFileName }),
+  saveExcel: (content: string, defaultFileName: string) => ipcRenderer.invoke('siai:save-excel', { content, defaultFileName }),
 });
