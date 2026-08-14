89a8bd863919b01b4de89df5c14858e7c09a883b
jeffer91 <jeffersonvillarreal91@gmail.com>
2026-08-14 11:45:31 -0500

    style: load phase 8 report styles

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import './phase4.css';
 import './external.css';
 import './phase6.css';
 import './phase7.css';
+import './phase8.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
